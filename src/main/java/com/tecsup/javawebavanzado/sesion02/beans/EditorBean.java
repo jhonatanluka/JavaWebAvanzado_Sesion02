1cc586dbf5b4b53684e3a9bfb9b79e3b806958bf
Alumno <Alumno@LAB1506-03>
2019-01-05 14:09:14 -0500

    hola

---
+++ src/main/java/com/tecsup/javawebavanzado/sesion02/beans/EditorBean.java
@@ -0,0 +1,24 @@
+package com.tecsup.javawebavanzado.sesion02.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class EditorBean {
+	private String comentarios;
+
+	public String getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public String grabar() {
+		System.out.println("grabando: " + comentarios);
+		return "test";
+	}
+
+}
